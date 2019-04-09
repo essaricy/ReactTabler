1fa6d1d850cbe3dd08e2bf9e0f7c2cc52b2e42e6
Srikanth Ragi <srikanthkumar.ragi@gmail.com>
2019-04-10 01:08:13 +0530

    Invoice screen work in progress

--- src/_routes/menu.routes.js
+++ src/_routes/menu.routes.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import CardScene from '../_scenes/card.scene';
-import CarousalScene from '../_scenes/carousal.scene';
-import CrudScene from '../_scenes/crud.scene';
 import InvoiceScene from '../_scenes/invoice.scene';
-import FormScene from '../_scenes/form.scene';
-import GalleryScene from '../_scenes/gallery.scene';
 import HomeScene from '../_scenes/home.scene';
-import InterfaceScene from '../_scenes/interface.scene';
-import ModalScene from '../_scenes/modal.scene';
 import ProfileScene from '../_scenes/profile.scene';
-import AlertScene from '../_scenes/alert.scene';
 
 export default class MenuRoutes extends React.Component {
   render() {
--- src/_scenes/invoice.scene.js
+++ src/_scenes/invoice.scene.js
@@ -1,9 +1,8 @@
-import React from "react";
-import ActionTableContainer from "../_containers/actiontable.container";
+import React from 'react';
+import ActionTableContainer from '../_containers/actiontable.container';
 
-import * as Urls from "../_constants/url.constant";
-import * as Events from "../_utils/event.util";
-import FormGroupContainer from "../_containers/formgroup.container";
+import * as Urls from '../_constants/url.constant';
+import * as Events from '../_utils/event.util';
 
 export default class InvoiceScene extends React.Component {
   constructor(props) {
@@ -13,160 +12,162 @@ export default class InvoiceScene extends React.Component {
   }
 
   render() {
-    return "";
-    // <ActionTableContainer
-    //   title="Invoices"
-    //   url={Urls.API_URL.BASE + Urls.API_URL.INVOICE}
-    //   columns={this.getColumns()}
-    //   actions={this.getActions()}
-    // />
+    return (
+      <ActionTableContainer
+        title="Invoices"
+        url={Urls.API_URL.BASE + Urls.API_URL.INVOICE}
+        columns={this.getColumns()}
+        actions={this.getActions()}
+      />
+    );
   }
 
-  // getColumns() {
-  //   return [
-  //     {
-  //       name: "Id",
-  //       field: "id",
-  //       hide: true
-  //     },
-  //     {
-  //       name: "Subject",
-  //       field: "subject",
-  //       sort: true,
-  //       render: data => (
-  //         <a href="/page/invoice" className="text-inherit">
-  //           {data.subject}
-  //         </a>
-  //       )
-  //     },
-  //     {
-  //       name: "Client",
-  //       field: "client",
-  //       sort: true
-  //     },
-  //     {
-  //       name: "Vat #",
-  //       field: "vat",
-  //       render: data => <span className="text-muted">{data.vat}</span>
-  //     },
-  //     {
-  //       name: "Created",
-  //       field: "created",
-  //       sort: true
-  //     },
-  //     {
-  //       name: "Status",
-  //       field: "status",
-  //       sort: true,
-  //       render: data => (
-  //         <span>
-  //           <span
-  //             className={"status-icon " + this.getStatusColor(data.status)}
-  //           />
-  //           {data.status}
-  //         </span>
-  //       )
-  //     },
-  //     {
-  //       name: "Price",
-  //       field: "price",
-  //       sort: true,
-  //       render: data => {
-  //         return "$" + data.price;
-  //       }
-  //     }
-  //   ];
-  // }
+  getColumns() {
+    return [
+      {
+        name: 'Id',
+        field: 'id',
+        hide: true
+      },
+      {
+        name: 'Subject',
+        field: 'subject',
+        sort: true,
+        render: data => (
+          <a href="/page/invoice" className="text-inherit">
+            {data.subject}
+          </a>
+        )
+      },
+      {
+        name: 'Client',
+        field: 'client',
+        sort: true
+      },
+      {
+        name: 'Vat #',
+        field: 'vat',
+        render: data => <span className="text-muted">{data.vat}</span>
+      },
+      {
+        name: 'Created',
+        field: 'created',
+        sort: true
+      },
+      {
+        name: 'Status',
+        field: 'status',
+        sort: true,
+        render: data => (
+          <span>
+            <span
+              className={'status-icon ' + this.getStatusColor(data.status)}
+            />
+            {data.status}
+          </span>
+        )
+      },
+      {
+        name: 'Price',
+        field: 'price',
+        sort: true,
+        render: data => {
+          return '$' + data.price;
+        }
+      }
+    ];
+  }
 
-  // getActions() {
-  //   return {
-  //     add: {
-  //       modalTitle: "Add New Invoice",
-  //       getEmpty: this.getEmpty,
-  //       content: this.getModalScene
-  //     },
-  //     update: {
-  //       modalTitle: "Update Invoice",
-  //       content: this.getModalScene
-  //     },
-  //     delete: {
-  //       message: "Do you really want to delete this invoice?"
-  //     }
-  //   };
-  // }
+  getActions() {
+    return {
+      add: {
+        modalTitle: 'Add New Invoice',
+        getEmpty: this.getEmpty,
+        content: this.getModalScene
+      },
+      update: {
+        modalTitle: 'Update Invoice',
+        content: this.getModalScene
+      },
+      delete: {
+        message: 'Do you really want to delete this invoice?'
+      }
+    };
+  }
 
-  // getEmpty() {
-  //   return {
-  //     id: 0,
-  //     subject: "Test subject",
-  //     client: "Test client",
-  //     vat: "87956421",
-  //     created: "24 Aug 2018",
-  //     status: "Pending",
-  //     price: 10
-  //   };
-  // }
+  getEmpty() {
+    return {
+      id: 0,
+      subject: 'Test subject',
+      client: 'Test client',
+      vat: '87956421',
+      created: '24 Aug 2018',
+      status: 'Pending',
+      price: 10
+    };
+  }
 
-  // getModalScene(data) {
-  //   return (
-  //     <div>
-  //       <input type="hidden" id="id" defaultValue={data.id} />
-  //       <FormGroupContainer label="Subject">
-  //         <Text
-  //           id="subject"
-  //           defaultValue={data.subject}
-  //           onChange={e => Events.setDataById(e, data)}
-  //         />
-  //       </FormGroupContainer>
-  //       <FormGroupContainer label="Client">
-  //         <Text
-  //           id="client"
-  //           defaultValue={data.client}
-  //           onChange={e => Events.setDataById(e, data)}
-  //         />
-  //       </FormGroupContainer>
-  //       <FormGroupContainer label="VAT#">
-  //         <Text
-  //           id="vat"
-  //           defaultValue={data.vat}
-  //           onChange={e => Events.setDataById(e, data)}
-  //         />
-  //       </FormGroupContainer>
-  //       <FormGroupContainer label="Created">
-  //         <Text
-  //           id="created"
-  //           defaultValue={data.created}
-  //           onChange={e => Events.setDataById(e, data)}
-  //         />
-  //       </FormGroupContainer>
-  //       <FormGroupContainer label="Status">
-  //         <Text
-  //           id="status"
-  //           defaultValue={data.status}
-  //           onChange={e => Events.setDataById(e, data)}
-  //         />
-  //       </FormGroupContainer>
-  //       <FormGroupContainer label="Price">
-  //         <Text
-  //           id="price"
-  //           defaultValue={data.price}
-  //           onChange={e => Events.setDataById(e, data)}
-  //         />
-  //       </FormGroupContainer>
-  //     </div>
-  //   );
-  // }
+  getModalScene(data) {
+    return '';
+    //   return (
+    //     <div>
+    //       <input type="hidden" id="id" defaultValue={data.id} />
+    //       <FormGroupContainer label="Subject">
+    //         <Text
+    //           id="subject"
+    //           defaultValue={data.subject}
+    //           onChange={e => Events.setDataById(e, data)}
+    //         />
+    //       </FormGroupContainer>
+    //       <FormGroupContainer label="Client">
+    //         <Text
+    //           id="client"
+    //           defaultValue={data.client}
+    //           onChange={e => Events.setDataById(e, data)}
+    //         />
+    //       </FormGroupContainer>
+    //       <FormGroupContainer label="VAT#">
+    //         <Text
+    //           id="vat"
+    //           defaultValue={data.vat}
+    //           onChange={e => Events.setDataById(e, data)}
+    //         />
+    //       </FormGroupContainer>
+    //       <FormGroupContainer label="Created">
+    //         <Text
+    //           id="created"
+    //           defaultValue={data.created}
+    //           onChange={e => Events.setDataById(e, data)}
+    //         />
+    //       </FormGroupContainer>
+    //       <FormGroupContainer label="Status">
+    //         <Text
+    //           id="status"
+    //           defaultValue={data.status}
+    //           onChange={e => Events.setDataById(e, data)}
+    //         />
+    //       </FormGroupContainer>
+    //       <FormGroupContainer label="Price">
+    //         <Text
+    //           id="price"
+    //           defaultValue={data.price}
+    //           onChange={e => Events.setDataById(e, data)}
+    //         />
+    //       </FormGroupContainer>
+    //     </div>
+    //   );
+  }
 
   getStatusColor(status) {
     let statusColor;
-    if (status === "Paid" || status === "Paid Today") {
-      statusColor = "bg-success";
-    } else if (status === "Pending") {
-      statusColor = "bg-danger";
-    } else if (status === "Due in 2 Weeks") {
-      statusColor = "bg-warning";
+    if (status === 'Paid' || status === 'Paid Today') {
+      statusColor = 'bg-success';
+    } else if (status === 'Pending') {
+      statusColor = 'bg-danger';
+    } else if (status === 'Due in 2 Weeks') {
+      statusColor = 'bg-warning';
     } else {
-      statusColor = "bg-grey";
+      statusColor = 'bg-grey';
     }
     return statusColor;
   }
